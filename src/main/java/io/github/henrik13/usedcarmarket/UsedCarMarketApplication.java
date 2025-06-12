7371dec89f043610fe3b1bf801eebbfdfc165e86
Henrik-13 <balint.henrik13@gmail.com>
2025-06-12 23:01:24 +0300

    init project

---
+++ src/main/java/io/github/henrik13/usedcarmarket/UsedCarMarketApplication.java
@@ -0,0 +1,13 @@
+package io.github.henrik13.usedcarmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsedCarMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsedCarMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/henrik13/usedcarmarket/UsedCarMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.henrik13.usedcarmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsedCarMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
